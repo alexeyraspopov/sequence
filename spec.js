0bdac4dce40c44465abba3a32fc36fbf013e1cca
Avenger7x <avenger7x13@gmail.com>
2014-03-23 18:13:38 +0200

    map and filter specs

--- spec.js
+++ spec.js
@@ -1,9 +1,53 @@
 /* global describe, it */
 'use strict';
-var should = require('should');
+var should, sequence;
+
+should = require('should');
+sequence = require('./index');
 
 describe('sequences', function(){
-	it('should pass', function(){
-		(true).should.be.exactly(true);
+	function inc(number){
+		return number + 1;
+	}
+
+	function isEven(number){
+		return number % 2 === 0;
+	}
+
+	it('should have mapper', function(){
+		var actual, expected;
+
+		actual = sequence.create([1, 2, 3])
+			.map(inc);
+
+		// [1, 2, 3] -> [2, 3, 4]
+		expected = [2, 3, 4];
+
+		actual.value().should.eql(expected);
+	});
+
+	it('should have filter', function(){
+		var actual, expected;
+
+		actual = sequence.create([1, 2, 3])
+			.filter(isEven);
+
+		// [1, 2, 3] -> [2]
+		expected = [2];
+
+		actual.value().should.eql(expected);
+	});
+
+	it('should have mapper and filter', function(){
+		var actual, expected;
+
+		actual = sequence.create([1, 2, 3])
+			.map(inc)
+			.filter(isEven);
+
+		// [1, 2, 3] -> [2, 3, 4] -> [2, 4]
+		expected = [2, 4];
+
+		actual.value().should.eql(expected);
 	});
 });
