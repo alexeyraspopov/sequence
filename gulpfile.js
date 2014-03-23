b037a1935d7e4038330ceb4db82da887b991b382
Avenger7x <avenger7x13@gmail.com>
2014-03-23 18:13:24 +0200

    dot reporter

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('analysis', function(){
 
 gulp.task('test', function(){
 	return gulp.src('spec.js')
-		.pipe(mocha({ reporter: 'list' }));
+		.pipe(mocha());
 });
 
 gulp.task('default', ['analysis', 'test']);
